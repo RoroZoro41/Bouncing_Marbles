220ede567360144c46c97132268e4b06feb45f84
RoroZoro41 <138931003+RoroZoro41@users.noreply.github.com>
2025-03-29 21:27:05 +0200

    pre-final project

--- bouncing-ball/Scripts/ball.gd
+++ bouncing-ball/Scripts/ball.gd
@@ -15,21 +15,24 @@ const GREEN_RED_SCREEN_BALL_SHADER = preload("res://Shaders/Ball Shaders/green r
 @export var push_force: float = 10000
 @export var min_size: float = 10
 @export var max_size: float = 50
+@export_range(0,1) var drag_strength: float = 0.5
 var ball_input_size: float = 10
 
+var is_inside_window_edges: bool = false
+
+#mouse bools
 var hovering_on_ball:bool = false
 var dragging_ball:bool = false
 
 
 func _ready() -> void:
-	#color = Color(randf(),randf(),randf())
 	var circle_collision:CircleShape2D = CircleShape2D.new()
-	# if ball too small, do random instead
+	# if ball too small, do the default ball size if 10
 	if (ball_input_size < 10):
-		#circle_collision.radius = randf_range(min_size, max_size)
 		return
 	else: circle_collision.radius = ball_input_size
-	#circle_collision.radius = max_size
+	
+	
 	collision_shape.shape = circle_collision
 	area_collision_shape.shape = collision_shape.shape
 	mass = circle_collision.radius/min_size
@@ -39,18 +42,12 @@ func _ready() -> void:
 		BLUE_RED_SCREEN_BALL_SHADER,
 		GREEN_BLUE_SCREEN_BALL_SHADER,
 		GREEN_RED_SCREEN_BALL_SHADER
-	]
+		]
 	if sprite.material:
-		#sprite.material.set("shader_parameter/marble_color", color)
 		#the 10.0 is because the sprite circle has a diameter of 10 pixels
 		sprite.material.set("shader_parameter/cirlce_size", 
-							collision_shape.shape.radius/10.0);
+						collision_shape.shape.radius/10.0);
 		sprite.material.shader = shaders.pick_random()
-
-	#applies random velocity upon creation
-	#apply_force(Vector2(
-					#randf_range(-push_force,push_force),
-					#randf_range(-push_force,push_force)))
 
 func _physics_process(_delta: float) -> void:
 	screen_bounce()
@@ -59,45 +56,54 @@ func _physics_process(_delta: float) -> void:
 func screen_bounce():
 # when the ball reaches either end of screen, flip their velocity, and play sound
 # Also, ball won't ever leave the window thanks to clamping the position
+
+	var radius = collision_shape.shape.radius
 	var window_edge = get_viewport_rect().size
 	var gpos = global_position
-	var radius = collision_shape.shape.radius
+	
+	#clamp forces the ball to teleport to window edge when outside it
 	position.x = clamp(gpos.x, radius ,window_edge.x-radius)
 	position.y = clamp(gpos.y, radius ,window_edge.y-radius)
+	#This checks if the ball is outside/touching the window edge
+		#It takes acount the ball's size/radius into account
 	if gpos.x - radius <= 0 or gpos.x + radius >= window_edge.x:
 		linear_velocity.x *= -1  # Reverse X velocity
-		if not dragging_ball:
+		if not dragging_ball and not is_inside_window_edges:
 			play_rand_ball_sound()
-	if gpos.y - radius <= 0 or gpos.y + radius >= window_edge.y:
+			is_inside_window_edges = true
+	#This checks if the ball is outside/touching the window edge
+		#It takes acount the ball's size/radius into account
+	elif gpos.y - radius <= 0 or gpos.y + radius >= window_edge.y:
 		linear_velocity.y *= -1  # Reverse Y velocity
-		if not dragging_ball:
+		if not dragging_ball and not is_inside_window_edges:
 			play_rand_ball_sound()
-	
+			is_inside_window_edges = true
+	else: is_inside_window_edges = false
 func drag():
-	#delete currently dragged ball
-	if dragging_ball:
-		if Input.is_action_just_pressed("Mouse_Press"):
-			queue_free()
-	# code for dragging the ball
+	#right click for drag
+	#if mouse is over the ball and you right clicked, then start dragging
+	if hovering_on_ball and Input.is_action_just_pressed("Right Mouse Button"):
+		dragging_ball = true
+	# stop dragging the ball when released the right mouse button
 	if Input.is_action_just_released("Right Mouse Button"):
 		dragging_ball = false
-	if hovering_on_ball:
-		if Input.is_action_just_pressed("Right Mouse Button"):
-			dragging_ball = true
+	#if you are draging and left clicked, delete currently dragged ball
+	if dragging_ball and Input.is_action_just_pressed("Mouse_Press"):
+		queue_free()
+	
+	#Drags the ball to the mouse position smoothly
 	if dragging_ball:
 		var mouse_pos = get_global_mouse_position()
 		var direction = (mouse_pos - global_position).normalized()
 		apply_force(direction * 1000 * Vector2(global_position).distance_to(mouse_pos))
-		linear_velocity = linear_velocity.lerp(Vector2.ZERO, 0.9)
+		linear_velocity = linear_velocity.lerp(Vector2.ZERO, drag_strength)
 
 func play_rand_ball_sound():
-	#The sound node2d
-	var sounds = $Sounds
+	var sounds = BallSounds
+	
 	#picking a random sound from the sound node
 	var rng = randi_range(0,sounds.get_child_count()-1)
-	
 	sounds.get_child(rng).pitch_scale = randf_range(0.95,1.4)
-	
 	sounds.get_child(rng).play()
 
 func _on_area_2d_mouse_entered() -> void:
--- bouncing-ball/Scripts/main.gd
+++ bouncing-ball/Scripts/main.gd
@@ -1,18 +1,13 @@
 extends Node2D
-const BALL = preload("res://Scenes/Ball.tscn")
+#const BALL = preload("res://Scenes/Ball.tscn")
 const BALL_WITH_SPRITE = preload("res://Scenes/ball_with_sprite.tscn")
 
 
 var mouse_start_position: Vector2 = Vector2(-9999,-9999)
 var mouse_end_position: Vector2 = Vector2(-9999,-9999)
-var ball_size_creation_multiplier: float = 0.5
+var ball_size_creation_multiplier: float = 0.5 #the correlation between how much you drag, and how bug the outline/ball will be
 
 func _process(_delta: float) -> void:
-	
-	#if Input.is_action_just_pressed("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button"):
-		#var ball: RigidBody2D = BALL_WITH_SPRITE.instantiate()
-		#ball.position = get_global_mouse_position()
-		#get_parent().add_child(ball)
 	if Input.is_action_just_pressed("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button"):
 		mouse_start_position = get_global_mouse_position()
 	if Input.is_action_pressed("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button"):
@@ -22,19 +17,22 @@ func _process(_delta: float) -> void:
 		#ball.position = mouse_start_position
 		ball.position.x = clamp(mouse_start_position.x, mouse_drag_distance()/2 + 1, get_viewport_rect().size.x - mouse_drag_distance()/2 - 1)
 		ball.position.y = clamp(mouse_start_position.y, mouse_drag_distance()/2 + 1 , get_viewport_rect().size.y - mouse_drag_distance()/2 - 1)
-
-# if circle too small, don't draw it
+		
+		# if circle too small, don't draw it
 		ball.ball_input_size = abs(mouse_drag_distance() * ball_size_creation_multiplier)
 		add_child(ball)
 	
 		#resetting the ball creation size
 		mouse_start_position = Vector2(-9999,-9999)
 		mouse_end_position = Vector2(-9999,-9999)
-	
+		
 	queue_redraw()
+	
 	
 func _draw() -> void:
 #draw a circle of center of where you first clicked
+	#if Input.is_action_pressed("Right Mouse Button"):
+		#pass
 	# of size of distance from how far you dragged
 	draw_circle(
 		mouse_start_position, #position
