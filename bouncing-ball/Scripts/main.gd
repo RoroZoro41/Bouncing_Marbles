3f05111e5993914b698eb27b394808c81bb4e7a8
RoroZoro41 <138931003+RoroZoro41@users.noreply.github.com>
2025-03-31 18:24:03 +0300

    The game is done and published to itch.io

--- bouncing-ball/Scripts/main.gd
+++ bouncing-ball/Scripts/main.gd
@@ -1,11 +1,18 @@
 extends Node2D
 #const BALL = preload("res://Scenes/Ball.tscn")
 const BALL_WITH_SPRITE = preload("res://Scenes/ball_with_sprite.tscn")
-
+@onready var sounds: Node = $Sounds
 
 var mouse_start_position: Vector2 = Vector2(-9999,-9999)
 var mouse_end_position: Vector2 = Vector2(-9999,-9999)
 var ball_size_creation_multiplier: float = 0.5 #the correlation between how much you drag, and how bug the outline/ball will be
+
+#func _ready() -> void:
+	#for i in 500:
+		#var ball: RigidBody2D = BALL_WITH_SPRITE.instantiate()
+		## if circle too small, don't draw it
+		#ball.ball_input_size = 10.0
+		#add_child(ball)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button"):
@@ -13,6 +20,7 @@ func _process(_delta: float) -> void:
 	if Input.is_action_pressed("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button"):
 		mouse_end_position = get_global_mouse_position()
 	if (Input.is_action_just_released("Mouse_Press") and not Input.is_action_pressed("Right Mouse Button")):
+		play_ball_spawn_sound()
 		var ball: RigidBody2D = BALL_WITH_SPRITE.instantiate()
 		#ball.position = mouse_start_position
 		ball.position.x = clamp(mouse_start_position.x, mouse_drag_distance()/2 + 1, get_viewport_rect().size.x - mouse_drag_distance()/2 - 1)
@@ -31,9 +39,7 @@ func _process(_delta: float) -> void:
 	
 func _draw() -> void:
 #draw a circle of center of where you first clicked
-	#if Input.is_action_pressed("Right Mouse Button"):
-		#pass
-	# of size of distance from how far you dragged
+	# of size being the distance from how far you dragged
 	draw_circle(
 		mouse_start_position, #position
 		abs(mouse_drag_distance() * ball_size_creation_multiplier), #radius
@@ -42,6 +48,12 @@ func _draw() -> void:
 		5 #line thickness
 	)
 
+func play_ball_spawn_sound():
+	var spawnSoundIndex :int = 2
+	#sounds.get_child(spawnSoundIndex)
+	sounds.get_child(spawnSoundIndex).pitch_scale = randf_range(0.5,2)
+	sounds.get_child(spawnSoundIndex).play()
+
 func mouse_drag_distance() -> float: 
 	return mouse_start_position.distance_to(mouse_end_position)
 
@@ -49,6 +61,5 @@ func mouse_drag_distance() -> float:
 func _on_button_pressed() -> void:
 	await get_tree().create_timer(0.01).timeout
 	for child in get_children():
-		print (child)
 		if child is RigidBody2D:
 			child.queue_free()
