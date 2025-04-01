edbc364484b7fd67f60d644379ae29987a9a33e3
RoroZoro41 <138931003+RoroZoro41@users.noreply.github.com>
2025-04-01 14:10:19 +0300

    Delete main.gd

--- main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-const BALL = preload("res://Ball.tscn")
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Mouse_Press") :
-		var ball: RigidBody2D = BALL.instantiate()
-		ball.position = get_global_mouse_position()
-		get_parent().add_child(ball)
